7074d725d5640fcd148965ae332e03ec6e8b2fcd
josecarlossantos <carlos.avellar@gmail.com>
2018-01-22 10:59:10 -0200

    JS OOP Initial

---
+++ app.js
@@ -0,0 +1,69 @@
+function onReady() {
+    createClock('clock');
+                createClock('clock2');
+
+}
+
+function createClock(id) {
+    var c = new Object();
+    c.updateClock = function () {
+        var date = new Date();
+        document.getElementById(id).innerHTML =
+            formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
+    }
+    setInterval(function () {
+        c.updateClock();
+    }, 1000);
+
+}
+
+function formatDigits(v) {
+    if (v < 10) {
+        v = '0' + v;
+    }
+    return v;
+}
+
+
+Window.onload = onReady();
+
+
+
+
+///**
+// * Created by josecarlossantos on 10/16/17.
+// */
+//
+//function onReady(){
+//
+//
+//    createClock();
+//
+//    function createClock(){
+//        var c = new Date();
+//            c.updateClock = function(){
+//
+//            var date = new Date();
+//            var clock = document.getElementById("clock");
+//            clock.innerHTML = formatDigits(date.getHours()) + ":" + formatDigits(date.getMinutes()) + ":" + formatDigits(date.getSeconds());
+//
+//            };
+//            setInterval(c.updateClock, 1000);
+//            c.updateClock();
+//
+//
+//    }
+//
+//
+//    function formatDigits(val){
+//       if(val < 10){
+//           val = "0" + val;
+//       }
+//       return val;
+//
+//    }
+//
+//
+//}
+//
+//Window.onload = onReady();
