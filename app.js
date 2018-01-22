ae8b22e338ec7da226b78bcf1065fdca0546b2e4
josecarlossantos <carlos.avellar@gmail.com>
2018-01-22 11:22:05 -0200

    JS OOP - Adding Foundations Files

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-function onReady() {
-    createClock('clock');
-                createClock('clock2');
+
+// The clock
+
+function onReady (){
+
+    consop
 
 }
 
-function createClock(id) {
-    var c = new Object();
-    c.updateClock = function () {
-        var date = new Date();
-        document.getElementById(id).innerHTML =
-            formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
-    }
-    setInterval(function () {
-        c.updateClock();
-    }, 1000);
 
-}
 
-function formatDigits(v) {
-    if (v < 10) {
-        v = '0' + v;
-    }
-    return v;
-}
+
+
+
+
+
+
+
+
+
+
+// function onReady() {
+//     createClock('clock');
+//                 createClock('clock2');
+//
+// }
+//
+// function createClock(id) {
+//     var c = new Object();
+//     c.updateClock = function () {
+//         var date = new Date();
+//         document.getElementById(id).innerHTML =
+//             formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
+//     }
+//     setInterval(function () {
+//         c.updateClock();
+//     }, 1000);
+//
+// }
+//
+// function formatDigits(v) {
+//     if (v < 10) {
+//         v = '0' + v;
+//     }
+//     return v;
+// }
 
 
 Window.onload = onReady();
