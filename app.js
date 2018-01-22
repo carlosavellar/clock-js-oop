80354fbd0887ea74feb429f8318877aaa62d81d5
josecarlossantos <carlos.avellar@gmail.com>
2018-01-22 19:02:14 -0200

    JS OOP - Click made with constructor

--- app.js
+++ app.js
@@ -1,22 +1,77 @@
 
-// The clock
+function onReady(){
+    console.log('Stating clock');
+    var clock = new Clock('clock');
+    var clock2 = new Clock('clock2');
+}
 
-function onReady (){
+function Clock(id){
 
-    console.log('Object Oriented programing');
+        this.updateClock = function () {
+            var data = new Date;
+
+            var clock = document.getElementById(id);
+            clock.innerHTML = this.formatDigits(data.getHours()) + ":" + this.formatDigits(data.getMinutes()) +
+                ":" + this.formatDigits(data.getSeconds());
+        }
+        this.formatDigits = function(val){
+            if(val < 10){
+                val = '0' + val;
+            }
+            return val;
+        }
+        // that was created to have acess to the function
+        var that = this;
+        setInterval(function(){
+            that.updateClock();
+            console.log("Teste");
+            }, 1000);
+        that.updateClock();
 
 }
 
 
-
-
-Window.onload(onReady);
-
+window.onload = onReady();
 
 
 
+///////////////////////// Section 2 Video 8
 
 
+// function onReady(){
+//     console.log('Stating clock');
+//     var clock = createClock('clock');
+//     var clock2 = createClock('clock2');
+// }
+//
+// function createClock(id){
+//     var d = {
+//         updateClock: function () {
+//             var data = new Date;
+//
+//             var clock = document.getElementById(id);
+//             clock.innerHTML = this.formatDigits(data.getHours()) + ":" + this.formatDigits(data.getMinutes()) +
+//                 ":" + this.formatDigits(data.getSeconds());
+//         },
+//         formatDigits: function(val){
+//             if(val < 10){
+//                 val = '0' + val;
+//             }
+//             return val;
+//         }
+//     };
+//     setInterval(function(){
+//         d.updateClock();
+//     }, 1000);
+//     d.updateClock();
+//     return d;
+// }
+//
+//
+// window.onload = onReady();
+
+
+///////////////////////// Section 2 Video 7
 
 
 
@@ -27,6 +82,9 @@ Window.onload(onReady);
 //
 // }
 //
+
+
+
 // function createClock(id) {
 //     var c = new Object();
 //     c.updateClock = function () {
@@ -48,10 +106,7 @@ Window.onload(onReady);
 // }
 
 
-Window.onload = onReady();
-
-
-
+// Window.onload = onReady();
 
 ///**
 // * Created by josecarlossantos on 10/16/17.
@@ -90,3 +145,5 @@ Window.onload = onReady();
 //}
 //
 //Window.onload = onReady();
+
+///////////////////////// Section 1 Video 6
